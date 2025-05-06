a32bdd9fa0e3ee35c1f047264ea0cf5ae9d101f6
Lee Yeon <rofttm0033@gmail.com>
2025-05-06 21:50:26 +0900

    feature: jpa

---
+++ src/main/java/umc/spring_study/service/StoreService/StoreQueryService.java
@@ -0,0 +1,11 @@
+package umc.spring_study.service.StoreService;
+
+import umc.spring_study.domain.Store;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StoreQueryService {
+   Optional<Store> findStore(Long id);
+   List<Store> findStoresByNameAndScore(String name, float score);
+}
---
+++ src/main/java/umc/spring_study/service/StoreService/StoreQueryServiceImpl.java
@@ -0,0 +1,33 @@
+package umc.spring_study.service.StoreService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import umc.spring_study.domain.Store;
+import umc.spring_study.repository.StoreRepository.StoreRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StoreQueryServiceImpl implements StoreQueryService {
+
+    private final StoreRepository storeRepository;
+
+
+    @Override
+    public Optional<Store> findStore(Long id) {
+        return storeRepository.findById(id);
+    }
+
+    @Override
+    public List<Store> findStoresByNameAndScore(String name, float score) {
+        List<Store> filteredStores = storeRepository.dynamicQueryWithBooleanBuilder(name, score);
+
+        filteredStores.forEach(store -> System.out.println("Store: " + store));
+
+        return filteredStores;
+    }
+}
